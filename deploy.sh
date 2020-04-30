6c4bf61f3838d8a4e38d67e0366e5059f505730d
Michael <mparas@gmail.com>
2020-04-30 19:17:05 +1000

    Fixed Docker Hub username reference

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 # Build images, tag each one, push to docker hub
 
-docker build -t michaelparaskakis/multi-client:latest -t mparaskakis/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t michaelparaskakis/multi-server:latest -t mparaskakis/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t michaelparaskakis/multi-worker:latest -t mparaskakis/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t mparaskakis/multi-client:latest -t mparaskakis/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mparaskakis/multi-server:latest -t mparaskakis/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mparaskakis/multi-worker:latest -t mparaskakis/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push michaelparaskakis/multi-client:latest
-docker push michaelparaskakis/multi-server:latest
-docker push michaelparaskakis/multi-worker:latest
+docker push mparaskakis/multi-client:latest
+docker push mparaskakis/multi-server:latest
+docker push mparaskakis/multi-worker:latest
 
-docker push michaelparaskakis/multi-client:$SHA
-docker push michaelparaskakis/multi-server:$SHA
-docker push michaelparaskakis/multi-worker:$SHA
+docker push mparaskakis/multi-client:$SHA
+docker push mparaskakis/multi-server:$SHA
+docker push mparaskakis/multi-worker:$SHA
 
 # Apply all configs in k8s folder
 
@@ -18,7 +18,7 @@ kubectl apply -f k8s
 
 # Imperatively set latest images on each deployment
 
-kubectl set image deployments/client-deployment client=michaelparaskakis/multi-client:$SHA
-kubectl set image deployments/server-deployment server=michaelparaskakis/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=michaelparaskakis/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=mparaskakis/multi-client:$SHA
+kubectl set image deployments/server-deployment server=mparaskakis/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=mparaskakis/multi-worker:$SHA
 
